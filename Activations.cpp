9ea1d32c7c3428ba9f1b3949302fce35151d454d
Osmar Vilchez Aguirre <osmar.vilchez@utec.edu.pe>
2026-04-05 00:49:04 -0500

    Refactor ReLU and Sigmoid activation functions

--- Activations.cpp
+++ Activations.cpp
@@ -3,27 +3,27 @@
 #include <algorithm>
 
 Tensor ReLU::apply(const Tensor& t) const {
-    Tensor result = t;
-
+    Tensor result = t; 
+    
     double* data = result.get_data();
     size_t size = result.get_total_size();
-
+    
     for (size_t i = 0; i < size; ++i) {
         data[i] = std::max(0.0, data[i]);
     }
-
+    
     return result;
 }
 
 Tensor Sigmoid::apply(const Tensor& t) const {
     Tensor result = t;
-
+    
     double* data = result.get_data();
     size_t size = result.get_total_size();
-
+    
     for (size_t i = 0; i < size; ++i) {
         data[i] = 1.0 / (1.0 + std::exp(-data[i]));
     }
-
+    
     return result;
 }
